ffe510ba963c043d351bea1a7c19df27ea3568a5
Leonardo P Monteiro <dev.pmonteiro@gmail.com>
2025-08-30 10:15:04 -0300

    att

---
+++ aula29/script.js
@@ -0,0 +1,6 @@
+const students = ['Francisco', 'Leonardo', 'Monteiro', 'Paiva']
+
+let newStudents = students.splice(2, 1)
+
+console.log(newStudents, ' Removidos')
+console.log(students, 'Lista final')
