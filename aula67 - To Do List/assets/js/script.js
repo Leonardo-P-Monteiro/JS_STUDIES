a94988c369d18e6d97be1cd197e394daa496ea17
Leonardo P Monteiro <dev.pmonteiro@gmail.com>
2026-02-19 10:44:21 -0300

    STANDARD COMMIT

---
+++ aula67 - To Do List/assets/js/script.js
@@ -0,0 +1,33 @@
+const inputTask = document.querySelector(".input-task");
+const btnTask = document.querySelector(".btn-task");
+const tasks = document.querySelector(".tasks");
+
+// PARAMOS NO MEIO DA AULA.
+
+// #4 Hearing the event of press key enter
+inputTask.addEventListener("keypress", function (e) {
+  if (!inputTask.value) {
+    return;
+  } else if (e.keyCode === 13) {
+    creatTask(inputTask.value);
+  }
+});
+
+// #3 Creating list item.
+const creatListItem = () => {
+  const li = document.createElement("li");
+  return li;
+};
+
+// #2 Inputting tasks on list to do.
+function creatTask(task) {
+  const li = creatListItem();
+  li.innerHTML = task;
+  tasks.appendChild(li);
+}
+
+// #1 Catching the button event
+btnTask.addEventListener("click", () => {
+  if (!inputTask.value) return;
+  creatTask(inputTask.value);
+});
