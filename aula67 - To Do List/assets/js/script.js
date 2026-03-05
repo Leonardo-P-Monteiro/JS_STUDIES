c6f9f5a69ff92d7e7b1877bd66c224563dc6266f
Leonardo P Monteiro <dev.pmonteiro@gmail.com>
2026-03-05 20:05:10 -0300

    STANDARD COMMIT

--- aula67 - To Do List/assets/js/script.js
+++ aula67 - To Do List/assets/js/script.js
@@ -20,16 +20,18 @@ const creatListItem = () => {
 };
 
 // #6 Creating the button delete task
-function createButtonDelete (li) {
-  li.innerHTML += ' ';
-  const buttonDelete = document.createElement('button');
-  buttonDelete.innerText = 'Apagar';
+function createButtonDelete(li) {
+  li.innerHTML += " ";
+  const buttonDelete = document.createElement("button");
+  buttonDelete.innerText = "Apagar";
+  buttonDelete.setAttribute("class", "apagar");
+  buttonDelete.setAttribute("title", "Apagar essa tarefa.");
   li.appendChild(buttonDelete);
 }
 
 // #5 cleaning the input field.
 const clearInput = () => {
-  inputTask.value = '';
+  inputTask.value = "";
   inputTask.focus();
 };
 
@@ -39,11 +41,20 @@ function creatTask(task) {
   li.innerHTML = task;
   tasks.appendChild(li);
   clearInput();
-  createButtonDelete(li)
+  createButtonDelete(li);
 }
 
 // #1 Catching the button event
 btnTask.addEventListener("click", () => {
   if (!inputTask.value) return;
   creatTask(inputTask.value);
+});
+
+// Listening the delete button.
+document.addEventListener("click", function (e) {
+  const element = e.target;
+
+  if (element.classList.contains("apagar")) {
+    element.parentElement.remove();
+  }
 });
